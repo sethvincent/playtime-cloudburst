6cb2ee9eb88a59d508b4b481589b4e07a992c423
sethvincent <sethvincent@gmail.com>
2013-02-28 16:23:47 -0800

    animation improvements

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var s = skrollr.init(/*other stuff*/);
 //The options (second parameter) are all optional. The values shown are the default values.
 skrollr.menu.init(s, {
     animate: true, //skrollr will smoothly animate to the new position using `animateTo`.
-    duration: 500, //How long the animation should take in ms.
-    easing: 'sqrt' //The easing function to use.
+    duration: 800, //How long the animation should take in ms.
+    easing: 'linear' //The easing function to use.
 });
